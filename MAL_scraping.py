4ca44572f94a6a30222c5cf2044b22ee20a13981
1234gary <ytgary.leung@mail.utoronto.ca>
2017-06-22 21:16:10 -0400

    Expanded types of recorded data

--- MAL_scraping.py
+++ MAL_scraping.py
@@ -35,14 +35,14 @@ import time
 
 #results are the variables I'm interested in, info are columns that share a similar 
 #structure in the html
-results = {"ID":[], "Title":[], "Type":[], "Episodes":[], "Status":[], "Aired":[], "Studios":[], "Source":[],
-           "Genres":[], "Duration":[], "Rating":[], "Ranked":[], "Popularity":[], "Score":[]}
-info = ["Type", "Episodes", "Status", "Aired", "Studios", "Source", "Genres", "Duration", "Rating", "Ranked", "Popularity"]
+results = {"ID":[], "Title":[], "Type":[], "Episodes":[], "Status":[], "Aired":[], "Premiered":[], "Broadcast":[],
+           "Producers":[], "Licensors":[], "Studios":[], "Source":[], "Genres":[], "Duration":[], "Rating":[],
+           "Score":[], "Ranked":[], "Popularity":[], "Members":[], "Favorites":[]}
+info = ["Type", "Episodes", "Status", "Aired", "Premiered", "Broadcast", "Producers", "Licensors", "Studios", "Source", "Genres",
+        "Duration", "Rating", "Score", "Ranked", "Popularity", "Members", "Favorites"]
 
 #MAL has a quite convenient characteristic that every page can be found using /anime/showid
 #where id is a number from 1 to some large number I don't know
-
-
 def get_mal_html():
     url = 'https://myanimelist.net/anime/'
     for attempt in range(10):
@@ -50,7 +50,7 @@ def get_mal_html():
             r = requests.get(url + str(i), headers={'User-agent': 'your bot 0.1'})  # so we don't get flagged as a bot
             return r.text
         except:
-            print("retrying")
+            print("Resending get request")
             time.sleep(5)
             continue
     raise ValueError('HTML failed to be obtained')
@@ -58,7 +58,7 @@ def get_mal_html():
 #this should go from 1 to 100000 (some arbitrarily large number)
 #however, I often change the range because the program crashes
 #edit: new try/except statement should fix most of these problems
-for i in range(0,150):
+for i in range(0,15):
     try:
         data = get_mal_html()
     except ValueError as err:
@@ -67,58 +67,53 @@ for i in range(0,150):
 
     title_start = data.index('<title>')
     title_end = data.index('</title>')
-
     title = (data[title_start+8:title_end-19])
+
     print(i)
     print((str(title.encode('utf-8')))[2:-1])
-    if "404 Not Found" not in data: #some pages return 404's, filter them out
-        results["ID"].append(i)
-        results["Title"].append((str(title.encode('utf-8')))[2:-1])
-        for i in info:
-            j=0
-            while j<100: 
-                try:
-                    info_text = '<span class="dark_text">' + i + ':</span>'
-                    info_start = (data.index(info_text))
-                    remaining = data[info_start: info_start + 1500]
-                    info_end = (remaining.index('/div'))
-                    j=100
-                except:
-                    j+=1
-                    time.sleep(5)
-                    pass
-            if i == "Premiered":
-                results[i].append((BeautifulSoup(remaining[3+len(info_text):info_end-16])).get_text())
-            elif i == "Broadcast":
-                results[i].append((BeautifulSoup(remaining[3+len(info_text):info_end-8])).get_text())
-            elif i == "Ranked":
-                results[i].append((BeautifulSoup(remaining[4+len(info_text):info_end-215])).get_text())
-                #print([int(s) for s in k.split() if s.isdigit()])
-            elif i == "Popularity":
-                results[i].append((BeautifulSoup(remaining[4+len(info_text):info_end-2])).get_text())
-            else:
-                results[i].append((BeautifulSoup(remaining[3+len(info_text):info_end-4])).get_text())
-        try:
-            score_text = '<span itemprop="ratingValue">'
-            score_start = data.index(score_text)
-            score = float(data[score_start+29:score_start+33])
-            results["Score"].append(score)
-        except:
-            score_text = '<span class="dark_text">Score:</span>' #some of the scores are formatted differently
-            score_start = data.index(score_text)
-            score = (data[score_start+46:score_start+50])
-            
-            
-            results["Score"].append(score)
 
+    if "404 Not Found" in data: #some pages return 404's, filter them out
+        continue
+
+    results["ID"].append(i)
+    results["Title"].append((str(title.encode('utf-8')))[2:-1])
+    for i in info:
+        info_text = '<span class="dark_text">' + i + ':</span>'
+        try:
+            info_start = (data.index(info_text))
+        except:
+            results[i].append("Unknown")
+            continue
+        remaining = data[info_start: info_start + 1500]
+        info_end = (remaining.index('/div'))
+
+        if i == "Premiered":
+            results[i].append((BeautifulSoup(remaining[3+len(info_text):info_end-16])).get_text())
+        elif i == "Broadcast":
+            results[i].append((BeautifulSoup(remaining[3+len(info_text):info_end-8])).get_text())
+        elif i == "Ranked":
+            results[i].append((BeautifulSoup(remaining[4+len(info_text):info_end-215])).get_text())
+            #print([int(s) for s in k.split() if s.isdigit()])
+        elif i == "Popularity":
+            results[i].append((BeautifulSoup(remaining[4+len(info_text):info_end-2])).get_text())
+        else:
+            results[i].append((BeautifulSoup(remaining[3+len(info_text):info_end-4])).get_text())
+    '''
+    try:
+        score_text = '<span itemprop="ratingValue">'
+        score_start = data.index(score_text)
+        score = float(data[score_start+29:score_start+33])
+        results["Score"].append(score)
+    except:
+        score_text = '<span class="dark_text">Score:</span>' #some of the scores are formatted differently
+        score_start = data.index(score_text)
+        score = (data[score_start+46:score_start+50])
+        results["Score"].append(score)
+    '''
 df = pd.DataFrame(results)
 df2 = df.set_index("ID")
 with open('mal_data.csv', 'a') as f:
     df2.to_csv(f, header=False)
-results = {"ID":[], "Title":[], "Type":[], "Episodes":[], "Status":[], "Aired":[], "Studios":[], "Source":[],
-            "Genres":[], "Duration":[], "Rating":[], "Ranked":[], "Popularity":[], "Score":[]}
-
-    #time.sleep(1)
 
 """
 #bebop testing
